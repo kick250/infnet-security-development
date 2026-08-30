3dde95cd24785c2decf635869fb74b7116b17c3b
Kick250 <kicklobat@gmail.com>
2026-08-30 14:30:53 -0300

    TP2: Exercise 7 login router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+# authentication
+from routes.authentication_router import AuthenticationRouter
+
 # api
 from routes.events_router import EventsRouter
 from routes.events_alternative_router import EventsAlternativeRouter
@@ -15,6 +18,9 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/health")
 def read_root():
     return { "status": "Server OK" }
+
+# authentication
+app.include_router(AuthenticationRouter.get_router())
 
 # api
 app.include_router(EventsRouter.get_router())
